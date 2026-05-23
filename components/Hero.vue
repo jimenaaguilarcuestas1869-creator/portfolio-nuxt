cb4ac047764b2bcc5a3349788f97b38aa4a238ed
Jimena Aguilar <jimenaaguilarcuestas1869@gmail.com>
2026-05-24 01:33:55 +0200

    feat: añadida animación de flotado con GSAP en sección Hero

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,3 +1,27 @@
+<script setup>
+import { onMounted } from 'vue'
+import { gsap } from 'gsap'
+
+onMounted(() => {
+  // Animación para el primer muñeco
+  gsap.to('.shape-1', {
+    y: -20,                // Sube 20 píxeles
+    duration: 2,           // Tarda 2 segundos en subir
+    yoyo: true,            // Cuando llega arriba, vuelve abajo
+    repeat: -1,            // Se repite infinitamente
+    ease: 'power1.inOut'   // Movimiento suave al arrancar y frenar
+  })
+
+  // Animación para el segundo muñeco (con tiempos ligeramente cambiados)
+  gsap.to('.shape-2', {
+    y: -15,                // Sube 15 píxeles
+    duration: 2.5,         // Tarda 2.5 segundos (descompasado para que quede más natural)
+    yoyo: true,
+    repeat: -1,
+    ease: 'power1.inOut'
+  })
+})
+</script>
 <template>
   <section class="hero">
     <div class="hero-content">
